8a4ad7ce1a8bdc0f9c94458ce10b0f484ed7e841
Chris Burrell <chris@burrell.me.uk>
2012-04-20 19:43:48 +0000

    some playing around in the last hour for mobile

--- step-web/src/main/webapp/js_mobile/init.js
+++ step-web/src/main/webapp/js_mobile/init.js
@@ -1,15 +1,60 @@
 init();
 
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+			function(m, key, value) {
+				vars[key] = value;
+			});
+	return vars;
+}
+
 function init() {
-	$(document).ready(function() {
-		
-		
-		var url = BIBLE_GET_BIBLE_TEXT + "KJV" + "/" + "Exodus 1";
-		
-		//send to server
-		$.get(url, function (text) {
-			//we get html back, so we insert into passage:
-			$("#mobileMainPane").html(text.value);
-		});
+	$(document).bind('pageinit', function() {
+		updatePassageView();
 	});
+
+	$(document).bind("pagechange", function(toPage, options) {
+		if (location.hash === "") {
+			// main page
+			updatePassageView();
+		}
+	});
+}
+
+/**
+ * a typical URL looks like this, so we construct it and then send it to the
+ * server:
+ * http://localhost:8080/step-web/rest/bible/getBibleText/KJV/Jhn%201:1/HEADINGS,VERSE_NUMBERS,STRONG_NUMBERS,MORPHOLOGY
+ */
+function updatePassageView() {
+	var params = getUrlVars();
+
+	var options = "";
+	var version = "KJV";
+	var passage = "Romans 1";
+
+	var url = BIBLE_GET_BIBLE_TEXT + version + "/" + passage + "/";
+
+	if (!params["headings"] || params["headings"] === "on") {
+		url += "HEADINGS,";
+	}
+
+	if (!params["verse_numbers"] || params["verse_numbers"] === "on") {
+		url += "VERSE_NUMBERS,";
+	}
+
+	if (params["strongs"] === "on") {
+		url += "STRONG_NUMBERS,";
+	}
+
+	if (params["morphs"] === "on") {
+		url += "MORPHOLOGY,";
+	}
+	
+	$.get(url, function(text) {
+		// we get html back, so we insert into passage:
+		$("#mobileMainPane").html(text.value);
+	});
+
 }

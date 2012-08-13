ee494d339f137b22cd39fcb43d3a2de73b030a74
chrisburrell <chris@burrell.me.uk>
2012-08-13 18:47:30 +0100

    TYNSTEP-206

--- step-web/src/main/webapp/js/defaults/step.defaults.js
+++ step-web/src/main/webapp/js/defaults/step.defaults.js
@@ -43,19 +43,15 @@ step.defaults = {
         textual : {
             sortByRelevance : true,
             availableRanges : [ { label: "Whole Bible",         value: "Gen-Rev" }, 
-                                { label: "Prophecy",           value: "Deu 28,Isa-Mal,Rev" },
                                 { label: "Old Testament",       value: "Gen-Mal" },
                                 { label: "New Testament",       value: "Mat-Rev" },
                                 { label: "The Pentateuch",      value: "Gen-Deu" },
-                                { label: "History",             value: "Josh-Est" },
-                                { label: "Poetry",              value: "Job-Song" },
-                                { label: "Major Prophets",      value: "Isa-Dan" },
-                                { label: "Minor Prophets",      value: "Hos-Mal" },
-                                { label: "Gospels",             value: "Mat-John" },
+                                { label: "History Books",       value: "Josh-Est" },
+                                { label: "Poetic Books",        value: "Job-Song" },
+                                { label: "Prophets",            value: "Isa-Mal" },
                                 { label: "Gospels and Acts",    value: "Mat-Acts" },
                                 { label: "Epistles",            value: "Rom-Rev" },
-                                { label: "Paul's Epistles",     value: "Rom-Phi" },
-                                { label: "Custom", value: "" }
+                                { label: "Pick books...", value: "" }
                               ]
         }
     }

bd32f2cffc9315f15ce7cb55634d62e478696e3f
Chris Burrell <chris@burrell.me.uk>
2014-05-12 19:15:06 +0100

    Fixes to make work without ?debug

--- step-web/src/main/webapp/js/require_config.js
+++ step-web/src/main/webapp/js/require_config.js
@@ -3,13 +3,14 @@ TTL_DAY = 86400000;
 requirejs.config({
     waitSeconds: 15000,
     paths : {
-        sidebar: ["js/sidebar-STEP_SERVER_VERSION_TOKEN.min"],
-        qtip : ["js/qtip-STEP_SERVER_VERSION_TOKEN.min"],
-        defaults: ["js/defaults/step.defaults-STEP_SERVER_VERSION_TOKEN.min"],
-        menu_extras: ["js/menu_extras-STEP_SERVER_VERSION_TOKEN.min"],
-        view_help_menu: ["js/help-menu-STEP_SERVER_VERSION_TOKEN.min"],
-        quick_lexicon : ["js/quick-lexicon-STEP_SERVER_VERSION_TOKEN.min"],
-        drag: ["js/draggabilly-STEP_SERVER_VERSION_TOKEN.min"],
-        html2canvas: ["js/html2canvas-STEP_SERVER_VERSION_TOKEN.min"]
+        sidebar: ["js/step.sidebar-STEP_SERVER_VERSION_TOKEN.min"],
+        defaults: ["js/step.defaults-STEP_SERVER_VERSION_TOKEN.min"],
+        quick_lexicon : ["js/step.quick-lexicon-STEP_SERVER_VERSION_TOKEN.min"],
+        qtip : ["js/step.qtip-STEP_SERVER_VERSION_TOKEN.min"],
+        view_help_menu: ["js/step.help-menu-STEP_SERVER_VERSION_TOKEN.min"],
+        drag: ["js/step.draggabilly-STEP_SERVER_VERSION_TOKEN.min"],
+        menu_extras: ["js/step.menu_extras-STEP_SERVER_VERSION_TOKEN.min"],
+        html2canvas: ["js/step.html2canvas-STEP_SERVER_VERSION_TOKEN.min"],
+        search: ["js/step.search-STEP_SERVER_VERSION_TOKEN.min"]
     }
 });

0ff9c264924afa430e200d90d491e2563253453f
David IB <technical@tyndale.cam.ac.uk>
2015-05-22 00:25:11 +0100

    Reduce the size of the Quick Lexicon box

--- step-web/src/main/webapp/js/backbone/views/view_quick_lexicon.js
+++ step-web/src/main/webapp/js/backbone/views/view_quick_lexicon.js
@@ -39,14 +39,15 @@ var QuickLexicon = Backbone.View.extend({
     templateDef: '<%= view.templateHeader %>' +
         '<% _.each(data, function(item) { %>' +
         '<div><h1>' +
-        '<%= item.stepGloss %> (<span class="transliteration"><%= item.stepTransliteration %></span> -- ' +
+        '<%= item.stepGloss %> (<span class="transliteration"><%= item.stepTransliteration %></span> - ' +
         '<span class="<%= fontClass %>"><%= item.accentedUnicode %></span>) ' +
         '</h1> ' +
         '<span class="shortDef"><%= item.shortDef == undefined ? "" : item.shortDef %></span>' +
         '<% if (item.shortDef == null || item.shortDef.length < 150) { %><div class="mediumDef"><%= item.mediumDef == undefined ? "" : item.mediumDef %></div> <% } %>' +
-        '<% if (item.count != null) { %><span class="strongCount"> (<%= sprintf(__s.stats_occurs_times_in_bible, item.count) %>)- <%= __s.more_info_on_click_of_word %></span><% } %>' +
+        '<% if (item.count != null) { %><span class="strongCount"> (<%= sprintf(__s.stats_occurs_times_in_bible, item.count) %>)</span><% } %>' +
         '</div>' +
         '<% }); %>' +
+        '<span class="infoTagLine"><%= __s.more_info_on_click_of_word %></span>' +
         '<%= view.templatedFooter %>',
     initialize: function (opts) {
         this.text = opts.text;

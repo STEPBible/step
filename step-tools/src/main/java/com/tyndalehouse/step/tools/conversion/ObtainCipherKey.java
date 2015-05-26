93e1546cda68b0da21f774e069af66672e54178c
Chris Burrell <chris@burrell.me.uk>
2015-05-26 16:18:56 +0100

    Changes to remove logging from encryption code.

--- step-tools/src/main/java/com/tyndalehouse/step/tools/conversion/ObtainCipherKey.java
+++ step-tools/src/main/java/com/tyndalehouse/step/tools/conversion/ObtainCipherKey.java
@@ -8,9 +8,6 @@ import org.jasypt.util.text.BasicTextEncryptor;
  */
 public class ObtainCipherKey {
     public static void main(String[] args) {
-        System.err.println("args to cipher key obfuscation");
-        System.err.println(args[0]);
-        System.err.println(args[1]);
         BasicTextEncryptor bte = new BasicTextEncryptor();
         bte.setPassword(args[0]);
         System.out.println(bte.encrypt(args[1]));

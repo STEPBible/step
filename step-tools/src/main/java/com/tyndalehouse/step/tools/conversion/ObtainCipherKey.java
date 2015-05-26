8d372d51e0407e497314ff777ab85278308caf9d
Chris Burrell <chris@burrell.me.uk>
2015-05-26 16:08:44 +0100

    Adding some logging

--- step-tools/src/main/java/com/tyndalehouse/step/tools/conversion/ObtainCipherKey.java
+++ step-tools/src/main/java/com/tyndalehouse/step/tools/conversion/ObtainCipherKey.java
@@ -8,6 +8,8 @@ import org.jasypt.util.text.BasicTextEncryptor;
  */
 public class ObtainCipherKey {
     public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
         BasicTextEncryptor bte = new BasicTextEncryptor();
         bte.setPassword(args[0]);
         System.out.println(bte.encrypt(args[1]));

d880ff409eba9e03d09203362277b21d9cdd63ec
Chris Burrell <chris@burrell.me.uk>
2015-05-26 16:10:18 +0100

    Encryption logging

--- step-tools/src/main/java/com/tyndalehouse/step/tools/conversion/ObtainCipherKey.java
+++ step-tools/src/main/java/com/tyndalehouse/step/tools/conversion/ObtainCipherKey.java
@@ -8,8 +8,9 @@ import org.jasypt.util.text.BasicTextEncryptor;
  */
 public class ObtainCipherKey {
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        System.out.println(args[1]);
+        System.err.println("args to cipher key obfuscation");
+        System.err.println(args[0]);
+        System.err.println(args[1]);
         BasicTextEncryptor bte = new BasicTextEncryptor();
         bte.setPassword(args[0]);
         System.out.println(bte.encrypt(args[1]));

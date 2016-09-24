913ae5177c13e133c3379aff7c5659c86299c0a4
Chris Burrell <chris@burrell.me.uk>
2016-09-24 19:03:41 +0100

    Fixing install script for linux to use symlinks to allow multiple users to use system.

--- step-assembly/src/main/assembly/post-install.sh
+++ step-assembly/src/main/assembly/post-install.sh
@@ -1,12 +1,12 @@
 echo "Starting post install script..." > /var/log/step-post-install.log
 
-
-echo "Moving files from sword" >> /var/log/step-post-install.log
 eval echo "Home directory is ~" >> /var/log/step-post-install.log
 
-mkdir -p ~/.sword >> /var/log/step-post-install.log
-rsync --remove-source-files -a /opt/step/homes/sword ~/.sword >> /var/log/step-post-install.log
+echo "Changing permissions to allow read/write to this directory"
+chmod -R 777 /opt/step/homes >> /var/log/step-post-install.log
 
-echo "Moving files from jsword" >> /var/log/step-post-install.log
-mkdir -p ~/.jsword
-rsync --remove-source-files -a /opt/step/homes/jsword ~/.jsword >> /var/log/step-post-install.log
+echo "Linking files" >> /var/log/step-post-install.log
+ln -s /opt/step/homes/sword ~/.sword >> /var/log/step-post-install.log
+ln -s /opt/step/homes/jsword ~/.jsword >> /var/log/step-post-install.log
+ln -s /opt/step/step.desktop ~/Desktop/step.desktop >> /var/log/step-post-install.log
+

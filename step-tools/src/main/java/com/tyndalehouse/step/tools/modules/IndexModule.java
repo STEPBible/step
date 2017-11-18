6e54969d2b3b79f32b592432174d82ef6798409d
chrisburrell <chris@burrell.me.uk>
2017-11-18 11:08:54 +0000

    Index module

--- step-tools/src/main/java/com/tyndalehouse/step/tools/modules/IndexModule.java
+++ step-tools/src/main/java/com/tyndalehouse/step/tools/modules/IndexModule.java
@@ -7,21 +7,28 @@ import com.google.inject.Provider;
 import com.tyndalehouse.step.core.guice.StepCoreModule;
 import com.tyndalehouse.step.core.models.ClientSession;
 import com.tyndalehouse.step.core.service.BibleInformationService;
+import com.tyndalehouse.step.core.service.jsword.JSwordModuleService;
+import org.crosswire.common.util.CWProject;
+import org.crosswire.jsword.book.sword.SwordBookPath;
+
+import java.io.File;
 
 /**
- * Indexes all modules
+ * Indexes a single module modules
  * 
  * @author chrisburrell
  *
  */
-public class IndexAll {
+public class IndexModule {
     /**
      * main method
      * 
      * @param args list of arguments
      */
     public static void main(final String[] args) {
-        final BibleInformationService instance = Guice.createInjector(new StepCoreModule(), new Module() {
+        CWProject.instance().setFrontendName("step");
+
+        final JSwordModuleService instance = Guice.createInjector(new StepCoreModule(), new Module() {
 
             @Override
             public void configure(final Binder binder) {
@@ -35,7 +42,10 @@ public class IndexAll {
                 });
             }
 
-        }).getInstance(BibleInformationService.class);
-        instance.indexAll();
+        }).getInstance(JSwordModuleService.class);
+        System.out.println("Indexing " + args[0]);
+        instance.index(args[0]);
+
+        System.out.println("Finished indexing " + args[0]);
     }
 }

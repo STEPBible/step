323ff8759593df06a5a2806a585cf5d6bee0d572
Patrick Tang <patricksptang@gmail.com>
2021-01-08 17:13:21 -0800

    updated the Linux post install message

--- step-assembly/src/main/assembly/post-install.sh
+++ step-assembly/src/main/assembly/post-install.sh
@@ -11,10 +11,11 @@ ln -sf /opt/step/homes/jsword ~/.jsword >> /var/log/step-post-install.log
 [ -d "~/Desktop" ] && ln -sf /opt/step/step.desktop ~/Desktop/step.desktop >> /var/log/step-post-install.log
 
 origUser=`pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1 | sed 's/(//' | sed 's/)//'`
-echo "Orig user is $origUser.  Adding links for $origUser home directory"
+echo "Orig user is $origUser.  Adding links for $origUser home directory."
 if [ ! -z "$origUser" ]
 then
     [ -d "/home/$origUser/Desktop" ] && [ ! -f "/home/$origUser/Desktop/step.desktop" ] && ln -s /opt/step/step.desktop /home/$origUser/Desktop/step.desktop  >> /var/log/step-post-install.log
     [ -d "/home/$origUser" ] && [ ! -h "/home/$origUser/.sword" ] && ln -s /opt/step/homes/sword /home/$origUser/.sword >> /var/log/step-post-install.log
     [ -d "/home/$origUser" ] && [ ! -h "/home/$origUser/.jsword" ] && ln -s /opt/step/homes/jsword /home/$origUser/.jsword >> /var/log/step-post-install.log
 fi
+echo "Click on the STEP icon on the desktop to start STEP.  If the icon is not available, enter \"step\" at the command line."

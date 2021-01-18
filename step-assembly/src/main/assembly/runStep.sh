61be3fe2ae84af2c57248ce76d93d5baa54780f5
Patrick Tang <patricksptang@gmail.com>
2021-01-18 10:56:16 -0800

    changes to linux installation

--- step-assembly/src/main/assembly/runStep.sh
+++ step-assembly/src/main/assembly/runStep.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
+user=`whoami`
+if [ "$user" = "root" ]; then
+	echo ""
+	echo "STEPBible should not run under root. Exiting ... Please run STEPBible with another user account."
+	exit
+fi
+userHome=$(awk -v u="$user" -v FS=':' '$1==u {print $6}' /etc/passwd)
+[ -z "$userHome" ] && userHome = "/home/$user"
+if [ ! -h "$userHome/.sword" ] || [ ! -h "$userHome/.jsword"	]; then
+   [ -d "$userHome/Desktop" ] && cp /opt/step/step.desktop $userHome/Desktop/step.desktop && chown $user: $userHome/Desktop/step.desktop
+fi
+ln -sf /opt/step/homes/sword $userHome/.sword
+ln -sf /opt/step/homes/jsword $userHome/.jsword
 /opt/step/step-install4j  > ~/step.log 2>&1 &

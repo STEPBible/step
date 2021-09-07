3621dad00c51cabcef80e197799b8d916af4e938
Patrick Tang <patricksptang@gmail.com>
2021-09-07 01:10:46 -0700

    highlight the Classical interface

--- step-web/src/main/webapp/js/backbone/views/view_examples.js
+++ step-web/src/main/webapp/js/backbone/views/view_examples.js
@@ -12,7 +12,7 @@ var ExamplesView = Backbone.View.extend({
 				'</h5>' +
 				'<div class="accordion-body">' +
 					'<br>' +
-					'<div id="classicalUIVideo"><span style="font-size:14px;font-weight:bold;color:black"><%= __s.display_classical_ui %></span>' +
+					'<div id="classicalUIVideo"><span style="font-size:14px;font-weight:bold;color:black"><mark><%= __s.display_classical_ui %></mark></span>' +
 					'<br><span class="explanationText"><%= __s.advanced_interface_explain %></span>' +
 					'<a href="javascript:step.util.showVideoModal(\'ClassicalUI.gif\', 23)">&nbsp;<span class="glyphicon glyphicon-film" style="font-size:16px"></span></a>' +
 					'</div>' +

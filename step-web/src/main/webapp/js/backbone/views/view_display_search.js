0853f2bc7181625c93071af33970a99b72086ac5
Patrick Tang <patricksptang@gmail.com>
2022-05-14 17:22:15 -0700

    removed blank lines

--- step-web/src/main/webapp/js/backbone/views/view_display_search.js
+++ step-web/src/main/webapp/js/backbone/views/view_display_search.js
@@ -356,11 +356,8 @@ var SearchDisplayView = DisplayView.extend({
         if (strongsList == undefined) {
             return;
         }
-
         //now let's iterate through the list of strongs, find all the elements that match, and add the highlight class
-
         for (var i = 0; i < strongsList.length; i++) {
-
             $("span[strong~='" + step.util.unaugmentStrong(strongsList[i]) + "']", results).addClass("secondaryBackground");
         }
     },

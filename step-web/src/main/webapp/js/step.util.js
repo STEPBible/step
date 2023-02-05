1a0f240eafbfbb812232d67a557a8867a0d638c5
Evan Tang <patricksptang@gmail.com>
2023-02-04 21:59:08 -0800

    only show the copy function to new users one time

--- step-web/src/main/webapp/js/step.util.js
+++ step-web/src/main/webapp/js/step.util.js
@@ -2729,7 +2729,7 @@ step.util = {
 		    var introCountFromStorageOrCookie = step.util.localStorageGetItem("step.copyIntro");
 			var introCount = parseInt(introCountFromStorageOrCookie, 10);
 			if (isNaN(introCount)) introCount = 0;
-			if ((introCount < 2) && (window.innerWidth > 499) && ($("#copy-icon").is(":visible"))) {
+			if ((introCount < 1) && (window.innerWidth > 499) && ($("#copy-icon").is(":visible"))) {
 				var introJsSteps = [
 				{
 					element: document.querySelector('#copy-icon'),

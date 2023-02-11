7e6a634b183528a6464ae6bf9d39de583d671576
Evan Tang <patricksptang@gmail.com>
2023-02-10 20:51:41 -0800

    test

--- step-web/src/main/java/com/tyndalehouse/step/models/WebSessionImpl.java
+++ step-web/src/main/java/com/tyndalehouse/step/models/WebSessionImpl.java
@@ -102,14 +102,16 @@ public class WebSessionImpl implements ClientSession {
             int size = received.available();
             byte[] encoded = new byte[size];
             size = received.read(encoded);
-            if(Base64.isArrayByteBase64(encoded)) {
-                byte[] decoded = decodeBase64(encoded);
-                return new ByteArrayInputStream(decoded);
-            }
-            else
-            {
-                return received;
-            }
+            //if(Base64.isArrayByteBase64(encoded)) {
+            byte[] decoded = java.util.Base64.getDecoder().decode(encoded);
+            return (new ByteArrayInputStream(decoded));
+//                byte[] decoded = decodeBase64(encoded);
+//                return new ByteArrayInputStream(decoded);
+//            }
+//            else
+//            {
+//                return received;
+//            }
         } catch (ServletException e) {
             throw new StepInternalException("Unable to obtain part", e);
         } catch (IOException e) {

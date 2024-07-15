927bcb67fd0230ec1461b867e9c3b1c6f18382d7
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2024-07-15 00:02:05 -0700

    Added commenrt

--- step-core/src/main/java/com/tyndalehouse/step/core/service/jsword/JSwordPassageService.java
+++ step-core/src/main/java/com/tyndalehouse/step/core/service/jsword/JSwordPassageService.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface JSwordPassageService {
     String REFERENCE_BOOK = "ESV_th";
     String SECONDARY_REFERENCE_BOOK = "NIV";
-    String OT_BOOK = "THOT"; // "OSHB";
+    String OT_BOOK = "THOT"; // Used to be "OSHB" until June 2024;
     String BEST_VERSIFICATION = "KJV";
     int MAX_VERSES_RETRIEVED = 200;
 

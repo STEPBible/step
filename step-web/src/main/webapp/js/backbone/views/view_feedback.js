e01434d0d1ca1322014334300f7bbd395b6f8fc4
Patrick Tang <patricksptang@gmail.com>
2024-10-22 00:37:32 -0700

    test

--- step-web/src/main/webapp/js/backbone/views/view_feedback.js
+++ step-web/src/main/webapp/js/backbone/views/view_feedback.js
@@ -92,7 +92,7 @@ var FeedbackView = Backbone.View.extend({
                 step.settings.save({ userFeedbackEmail: email }, {silent: true});
 
                 //formData.push({ key: "type", value: self.feedbackForm.find("#feedbackType").val()});
-                formData.push({ key: "type", "Bug"});
+                formData.push({ key: "type", value: "Bug"});
                 formData.push({ key: "email", value: email});
                 formData.push({ key: "description", value: self.feedbackForm.find("#feedbackDescription").val()});
                 formData.push({ key: "summary", value: self.feedbackForm.find("#feedbackSummary").val()});

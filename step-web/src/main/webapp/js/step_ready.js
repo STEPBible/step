3aa9cbcf1ad83cf0badc685de84ee6d36b9e0373
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2024-11-17 17:48:45 -0800

    fix

--- step-web/src/main/webapp/js/step_ready.js
+++ step-web/src/main/webapp/js/step_ready.js
@@ -499,7 +499,6 @@
         //iframe
         if (window != window.top) {
             step.util.showOrHideTutorial(true);
-            var urlPart1 = ;
             if (window.location.origin.indexOf("examples.stepbible.org") > -1) { // Don't want user to break out to examples.stepbible.org
                 var button = $("<button class='stepBreakout btn btn-default btn-xs'><span class='glyphicon glyphicon-new-window'></button>");
                 $(".headerButtons").append(button);

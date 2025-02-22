e5b806c8779b36914d901d12f7b251e328ae91bb
Patrick Tang <patricksptang@gmail.com>
2025-02-22 06:06:59 -0800

    delete files which has moved to step-tools directory

--- scripts/shelvesword.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-
-#swaps the user home on Windows
-mv $APPDATA/Sword $APPDATA/Sword.push
-mv $APPDATA/JSword $APPDATA/JSword.push
-mv $HOME/.STEP $HOME/.STEP.push
-
--- scripts/test-regex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-set -e
-
-echo "set 10"
-find -name "*.properties" -print | xargs sed -i 's/% \([0-9]\) \$ \([sd]\) /%\1$\2 /g'
-find -name "*.properties" -print | xargs sed -i 's/% \([0-9]\) \$ \([sd]\)$/%\1$\2/g'
-

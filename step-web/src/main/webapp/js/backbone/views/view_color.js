9ce5187460e21a7aff3f30a568d22e99022b2641
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2025-03-08 15:01:41 -0800

    fix

--- step-web/src/main/webapp/js/backbone/views/view_color.js
+++ step-web/src/main/webapp/js/backbone/views/view_color.js
@@ -31,6 +31,9 @@ var ColorView = Backbone.View.extend({
             var availableOptions = activePassage.get("options");
             if ((typeof availableOptions === "string") && (availableOptions.indexOf("C") > -1)) { // Color grammar is available
                 if (typeof c4 === "undefined") cf.initCanvasAndCssForClrCodeGrammar(); //c4 is currentClrCodeConfig.  It is called to c4 to save space
+                var C_Greek = 0; // This need to match definition in color_code_grammar.js
+                var C_enableVerbClr = 0; // This need to match definition in color_code_grammar.js
+                var C_enableGenderNumberClr = 3; // This need to match definition in color_code_grammar.js
                 if (c4[C_Greek][C_enableVerbClr] || // Enabled Greek verb color code 
                     c4[C_enableGenderNumberClr] ||  // Or enabled noun
                     c4[C_OT][C_enableVerbClr] )     // Or enabled OT verb color code 

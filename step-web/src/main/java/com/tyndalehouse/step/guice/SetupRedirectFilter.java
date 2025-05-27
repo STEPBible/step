c8903ca69837df0aed85944ea0b917c35f187056
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2025-05-26 21:07:39 -0700

    prevent and monitor XSS attack

--- step-web/src/main/java/com/tyndalehouse/step/guice/SetupRedirectFilter.java
+++ step-web/src/main/java/com/tyndalehouse/step/guice/SetupRedirectFilter.java
@@ -9,6 +9,8 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
 import java.util.Map;
+// import org.owasp.html.PolicyFactory;
+// import org.owasp.html.Sanitizers;
 
 /**
  * Intercepts and works out whether STEP has finished the installation process
@@ -18,6 +20,7 @@ public class SetupRedirectFilter implements Filter {
     //sourced from step.core.properties, identifies the version of the currently running application
     private String runningAppVersion;
     private AppManagerService appManager;
+//    private PolicyFactory policy;
 
     /**
      * @param runningAppVersion the version of the running application
@@ -31,6 +34,7 @@ public class SetupRedirectFilter implements Filter {
 
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
+ //       policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
     }
 
     @Override
@@ -44,11 +48,17 @@ public class SetupRedirectFilter implements Filter {
             String[] value = entry.getValue();
             for (int i = 0; i < value.length; i++) {
                 String checkValue = value[i].replaceAll("\\s+","") .toLowerCase();
+                //String safeHTML = policy.sanitize(checkValue);
+                //if (!safeHTML.equals(checkValue)) {
+                //    System.out.println("Unmatch safe: " + safeHTML + "\n orig: " + checkValue);
+                //}
                 if (checkValue.contains("script>") &&
                         (checkValue.contains("<script>") || checkValue.contains("</script>"))) {
                     System.out.println("XSS attack detected: " + key + "=" + value[i] + " uri: " + ((HttpServletRequestWrapper) request).getRequestURI());
-
+                    return;
                 }
+                if (checkValue.contains("<") || checkValue.contains("<"))
+                    System.out.println("XSS check: " + key + "=" + value[i] + " uri: " + ((HttpServletRequestWrapper) request).getRequestURI());
             }
         }
         if (!appManager.isLocal() || (installedVersion != null && installedVersion.equals(runningAppVersion))) {

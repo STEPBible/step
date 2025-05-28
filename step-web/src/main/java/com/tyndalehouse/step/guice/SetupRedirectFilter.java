a0a1c77c5d3cd5a7b819fa8601ab34adce753d27
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2025-05-28 15:18:06 -0700

    change to check XSS

--- step-web/src/main/java/com/tyndalehouse/step/guice/SetupRedirectFilter.java
+++ step-web/src/main/java/com/tyndalehouse/step/guice/SetupRedirectFilter.java
@@ -57,7 +57,7 @@ public class SetupRedirectFilter implements Filter {
                 }
                 if (checkValue.contains("<") || checkValue.contains(">") || checkValue.contains("%3c") || checkValue.contains("%3e") ||
                         checkValue.contains("&lt") || checkValue.contains("&gt") ||
-                        checkValue.contains("#6") || checkValue.contains("#0") || checkValue.contains("#x") || checkValue.contains("003"))
+                        checkValue.contains("#6") || checkValue.contains("#0") || checkValue.contains("#x") || checkValue.contains("\u003c"))
                     System.out.println("XSS check: " + key + "=" + value[i] + " uri: " + ((HttpServletRequestWrapper) request).getRequestURI());
                 //String safeHTML = policy.sanitize(checkValue);
                 //if (!safeHTML.equals(checkValue)) {

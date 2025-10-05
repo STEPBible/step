e67b0661a61a7e6a2d00e245683d5c09f65ca072
nathanvercaemert <50712356+nathanvercaemert@users.noreply.github.com>
2025-10-05 17:15:21 -0400

    Use chevrons for genealogy timeline (#28) (#225)

--- step-web/src/main/webapp/js/J_AppsJs/J_Genealogy/j_genealogy.js
+++ step-web/src/main/webapp/js/J_AppsJs/J_Genealogy/j_genealogy.js
@@ -3129,7 +3129,9 @@ class _ClassPresentationHandler
 	    const fromAdamFivePercents = Math.round( (100 * (generationsFromAdam / 77)) / 5);
 	    const before = '&#x2588;'.repeat(fromAdamFivePercents);
 	    const after  = '&#x2588;'.repeat(20 - fromAdamFivePercents);
-	    generations = `<p><b>Timeline:</b> Adam &#x25C0; <span style='color:lightgray;font-size:small'>${before}</span>&#x2588;<span style='color:lightgray;font-size:small'>${after}</span> &#x25B6; NT</p>`;
+	    const leftChevronSvg  = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" style="width:0.9em;height:0.9em;vertical-align:middle;" aria-hidden="true"><polyline points="7.5 2 4.5 6 7.5 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
+	    const rightChevronSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" style="width:0.9em;height:0.9em;vertical-align:middle;" aria-hidden="true"><polyline points="4.5 2 7.5 6 4.5 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
+	    generations = `<p><b>Timeline:</b> Adam ${leftChevronSvg} <span style='color:lightgray;font-size:small'>${before}</span>&#x2588;<span style='color:lightgray;font-size:small'>${after}</span> ${rightChevronSvg} NT</p>`;
 	}
 
 

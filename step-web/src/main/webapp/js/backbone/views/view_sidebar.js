f32b416dcc88b22fded23711ae63249ef8833ece
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2026-06-27 17:24:07 -0700

    Minor change

--- step-web/src/main/webapp/js/backbone/views/view_sidebar.js
+++ step-web/src/main/webapp/js/backbone/views/view_sidebar.js
@@ -1227,8 +1227,7 @@ var SidebarView = Backbone.View.extend({
         var decomposed = str.normalize('NFD');
         // 2. \p{Mn}: Matches the non-spacing combining marks (U+0300–U+036F)
         var regExPattern = "\\/\\\p{Mn}\\/gu"; // Need to put this in a variable because maven minifier does not recognize the "u" (unicode) option which was introduced in ES2015.
-        var firstChar = decomposed.replace(regExPattern, "");
-        firstChar = firstChar.substring(0,1);
+        var firstChar = decomposed.replace(regExPattern, "").substring(0,1);
         if (greekMap[firstChar])
             return greekMap[firstChar];
         console.log("unrecognized first char " + firstChar + " in " + str);

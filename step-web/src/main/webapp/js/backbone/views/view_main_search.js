ec460616350a210c3787c01afa43ac8b5df544f7
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2026-08-18 10:55:14 -0700

    Update for introJS

--- step-web/src/main/webapp/js/backbone/views/view_main_search.js
+++ step-web/src/main/webapp/js/backbone/views/view_main_search.js
@@ -624,11 +624,11 @@ var MainSearchView = Backbone.View.extend({
             args += refArgs;
         }
         step.router.navigateSearch(args);
-        if (numOfBibleVersions > 1) step.util.showIntroOfMultiVersion();
-        else if (((newMasterVersion === "NIV") || (newMasterVersion === "NIVUK") || 
+        if (((newMasterVersion === "NIV") || (newMasterVersion === "NIVUK") || 
             (otherVersions.indexOf("NIV") > -1) || (otherVersions.indexOf("NIVUK") > -1)) &&
             (step.util.bookOrderInBible(osisIds[0]) > 38))
             step.util.showIntroOfTaggedNIVNT();
+        else if (numOfBibleVersions > 1) step.util.showIntroOfMultiVersion();
     },
     getCurrentInput: function () {
         return this.masterSearch.select2("container").find(".select2-input").val();

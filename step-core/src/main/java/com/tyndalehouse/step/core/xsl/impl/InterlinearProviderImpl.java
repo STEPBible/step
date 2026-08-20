a8d2389b7005e9bc6f0fbec629e2d983c9c05478
Patrick Tang <36460266+patricksptang@users.noreply.github.com>
2026-08-20 00:02:17 -0700

    Do not apply interlinear to H9* Strongs

--- step-core/src/main/java/com/tyndalehouse/step/core/xsl/impl/InterlinearProviderImpl.java
+++ step-core/src/main/java/com/tyndalehouse/step/core/xsl/impl/InterlinearProviderImpl.java
@@ -494,7 +494,17 @@ public class InterlinearProviderImpl implements InterlinearProvider {
         // have one phrase anyway
         final List<Word> words = new ArrayList<Word>(2);
         boolean partial = false;
+        boolean hasStrongNotStartWithH9 = false;
         for (int ii = 0; ii < strongs.length; ii++) {
+            if (strongs[ii].startsWith("strong:H9") && strongs[ii].startsWith("H0413"))
+                continue;
+            hasStrongNotStartWithH9 = true;
+            break;
+        }
+
+        for (int ii = 0; ii < strongs.length; ii++) {
+            if (hasStrongNotStartWithH9 && (strongs[ii].startsWith("strong:H9") || strongs[ii].startsWith("H0413")))
+                continue;
             final String strongKey = getAnyKey(strongs[ii]);
             if (!isH00(strongKey) && !blacklisted(strongKey)) {
                 words.add(addTextualInfo(currentVerse, strongKey, word, untaggedContent));
